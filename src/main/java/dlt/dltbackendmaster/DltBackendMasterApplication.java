b5d65f47c92b74e9e0a9efab2321b76ae97e2217
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2021-11-09 16:54:16 +0200

    Initial Maven project skeletun

---
+++ src/main/java/dlt/dltbackendmaster/DltBackendMasterApplication.java
@@ -0,0 +1,13 @@
+package dlt.dltbackendmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DltBackendMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DltBackendMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dlt/dltbackendmaster/DltBackendMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package dlt.dltbackendmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DltBackendMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

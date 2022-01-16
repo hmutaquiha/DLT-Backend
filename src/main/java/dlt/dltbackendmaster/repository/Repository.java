03b168c9e07ec6d0753461836dfa25d104ca6070
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-01-16 22:54:52 +0200

    added hibernate DAO repository interface and implementations

---
+++ src/main/java/dlt/dltbackendmaster/repository/Repository.java
@@ -0,0 +1,39 @@
+package dlt.dltbackendmaster.repository;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This interface represents the integrated Repository for all domains 
+ * @author derciobucuane
+ *
+ */
+public interface Repository {
+	<T> List<T> getAll(Class<T> klass);
+
+    <T> List<T> getAllQuery(String s);
+
+    <T> void update(T klass);
+
+    <T> boolean exist(T klass);
+    
+    <T> int updateQuery(String query, Object... params);
+    
+    <T> int count(Class<T> klass);
+
+    <T> void Save(T klass);
+
+    <T> T GetUniqueEntityByNamedQuery(String query, Object... params);
+    
+    <T> List<T> GetAllEntityByNamedQuery(String query, Object... params);
+
+    <T> List<T> findByQuery(String hql, Map<String, Object> entidade, Map<String, Object> namedParams);
+    
+    <T> List<T> findByQueryFilter(String hql, Map<String, Object> entidade, Map<String, Object> namedParams, int f, int m);
+    
+    <T> List<T> findByJPQuery(String hql,Map<String, Object> namedParams);
+    
+    <T> List<T> findByJPQueryFilter(String hql, Map<String, Object> namedParams, int f, int m);
+
+    <T> void delete(T klass);
+}

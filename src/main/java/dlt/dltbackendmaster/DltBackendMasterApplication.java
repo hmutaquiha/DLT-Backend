3f64d354174278b48f210a737ef461272473c5d4
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-01-18 13:45:31 +0200

    DLT-64: added dependencies and register service bean

--- src/main/java/dlt/dltbackendmaster/DltBackendMasterApplication.java
+++ src/main/java/dlt/dltbackendmaster/DltBackendMasterApplication.java
@@ -2,8 +2,10 @@ package dlt.dltbackendmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"dlt.dltbackendmaster.service.Service"})
 public class DltBackendMasterApplication {
 
 	public static void main(String[] args) {

b24b7f10010ec5e76442f62455905e123d0eb092
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-01-19 23:20:50 +0200

    DLT-73: implement security Authentication class

---
+++ src/main/java/dlt/dltbackendmaster/security/UserAuthentication.java
@@ -0,0 +1,58 @@
+package dlt.dltbackendmaster.security;
+
+import java.util.Collection;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import dlt.dltbackendmaster.domain.Users;
+
+public class UserAuthentication implements Authentication{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final Users account;
+	private boolean authenticated = true;
+
+    public UserAuthentication(Users account) {
+        this.account = account;
+    }
+
+	@Override
+	public String getName() {
+		return account.getUsername();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return account.getAuthorities();
+	}
+
+	@Override
+	public Object getCredentials() {
+		return account.getPassword();
+	}
+
+	@Override
+	public Object getDetails() {
+		return account;
+	}
+
+	@Override
+	public Object getPrincipal() {
+		return account;
+	}
+
+	@Override
+	public boolean isAuthenticated() {
+		return authenticated;
+	}
+
+	@Override
+	public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+		this.authenticated = isAuthenticated;
+	}
+
+}

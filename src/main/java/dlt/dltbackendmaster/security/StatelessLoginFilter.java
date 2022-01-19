50c6f097676b1044a0f1abaf990e38d9e05aed21
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-01-20 00:25:45 +0200

    DLT-73: implemented security AbstractAuthenticationProcessingFilter

---
+++ src/main/java/dlt/dltbackendmaster/security/StatelessLoginFilter.java
@@ -0,0 +1,54 @@
+package dlt.dltbackendmaster.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import dlt.dltbackendmaster.domain.Users;
+
+public class StatelessLoginFilter extends AbstractAuthenticationProcessingFilter{
+
+	private final TokenAuthenticationService tokenAuthenticationService;
+	private UserDetailsServiceImpl userServiceImpl;
+
+	protected StatelessLoginFilter(String urlMapping, TokenAuthenticationService tokenAuthenticationService, 
+			UserDetailsServiceImpl userDetailsService, AuthenticationManager authManager) {
+		super(new AntPathRequestMatcher(urlMapping));
+		this.userServiceImpl = userDetailsService;
+		this.tokenAuthenticationService = tokenAuthenticationService;
+		setAuthenticationManager(authManager);
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException, IOException, ServletException {
+		
+		String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        final UsernamePasswordAuthenticationToken loginToken = new UsernamePasswordAuthenticationToken(username, password);
+        
+		return getAuthenticationManager().authenticate(loginToken);
+	}
+	
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+		final Users authenticatedUser = userServiceImpl.loadUserByUsername(authResult.getName());
+		final UserAuthentication userAuthentication = new UserAuthentication(authenticatedUser);
+		tokenAuthenticationService.addAuthentication(response, userAuthentication);
+		SecurityContextHolder.getContext().setAuthentication(userAuthentication);
+	}
+	
+	
+
+}

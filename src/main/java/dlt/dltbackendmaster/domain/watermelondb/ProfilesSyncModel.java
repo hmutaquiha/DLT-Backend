013cf80160a56ca06dbfda3ceec9aa82885f7f3d
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-04-11 10:09:06 +0200

    added sync models for domains

---
+++ src/main/java/dlt/dltbackendmaster/domain/watermelondb/ProfilesSyncModel.java
@@ -0,0 +1,73 @@
+package dlt.dltbackendmaster.domain.watermelondb;
+
+public class ProfilesSyncModel {
+
+	private String id;
+	private String _status;
+	private String _changed;
+	private String name;
+	private String description;
+	private int status;
+	private Integer online_id;
+	
+	public ProfilesSyncModel() {}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String get_status() {
+		return _status;
+	}
+
+	public void set_status(String _status) {
+		this._status = _status;
+	}
+
+	public String get_changed() {
+		return _changed;
+	}
+
+	public void set_changed(String _changed) {
+		this._changed = _changed;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+
+	public Integer getOnline_id() {
+		return online_id;
+	}
+
+	public void setOnline_id(Integer online_id) {
+		this.online_id = online_id;
+	}
+	
+	
+	
+}

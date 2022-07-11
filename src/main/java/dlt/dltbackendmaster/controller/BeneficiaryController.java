1a0031b42fb5331e7eb7376b08473d89c1fcf4ae
Hamilton Mutaquiha <hamilton.mutaquiha@gmail.com>
2022-07-11 20:01:38 +0200

    Added save and update endpoints

--- src/main/java/dlt/dltbackendmaster/controller/BeneficiaryController.java
+++ src/main/java/dlt/dltbackendmaster/controller/BeneficiaryController.java
@@ -2,22 +2,18 @@ package dlt.dltbackendmaster.controller;
 
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dlt.dltbackendmaster.domain.Beneficiaries;
-import dlt.dltbackendmaster.domain.Neighborhood;
-import dlt.dltbackendmaster.domain.Partners;
-import dlt.dltbackendmaster.domain.Users;
-import dlt.dltbackendmaster.domain.watermelondb.BeneficiarySyncModel;
 import dlt.dltbackendmaster.service.DAOService;
 
 @RestController
@@ -37,9 +33,8 @@ public class BeneficiaryController
             List<Beneficiaries> beneficiaries = service.getAll(Beneficiaries.class);
             return new ResponseEntity<List<Beneficiaries>>(beneficiaries, HttpStatus.OK);
         } catch (Exception e) {
-        	//e.printStackTrace();
-        	
-        	System.out.println(e.getMessage());
+            // e.printStackTrace();
+            System.out.println(e.getMessage());
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
@@ -52,46 +47,46 @@ public class BeneficiaryController
         }
 
         try {
-        	Beneficiaries beneficiary = service.find(Beneficiaries.class, id);
+            Beneficiaries beneficiary = service.find(Beneficiaries.class, id);
             return new ResponseEntity<>(beneficiary, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
-    
+
     @PostMapping(consumes = "application/json")
-    public ResponseEntity<Beneficiaries> save(@RequestBody BeneficiarySyncModel beneficiaryParam) {
-    	//System.out.println(beneficiary);
-    	
-    	/*Random rnd = new Random();
-        int nui = rnd.nextInt(99999);
-        
-        Beneficiary beneficiary = new Beneficiary(beneficiaryParam);
-        beneficiary.setNui(""+nui);
-    	beneficiary.setNeighborhood(new Neighborhood(1));
-    	//Partners partners = new Partners();
-    	//partners.setId(1);
-    	//beneficiary.setPartner(partners);
-    	beneficiary.setDateCreated(new Date());
-    	beneficiary.setName(beneficiaryParam.getNick_name());
-    	beneficiary.setStatus(1);
-    	Users createdBy = new Users();
-    	createdBy.setId(1);
-    	beneficiary.setCreatedBy(createdBy);
-    	beneficiary.setUsId(1);
-    	*/
-    	
-    	try {
-		//	service.Save(beneficiary);
-			
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-    	
-    	
-    	return new ResponseEntity<>(null, HttpStatus.OK);
-    	
+    public ResponseEntity<Beneficiaries> save(@RequestBody Beneficiaries beneficiary) {
+
+        if (beneficiary == null) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            beneficiary.setDateCreated(new Date());
+            // TODO: Incluir algoritmo para geração de NUI
+            beneficiary.setNui("0401/0000000016");
+            Integer beneficiaryId = (Integer) service.Save(beneficiary);
+            Beneficiaries createdBeneficiary = service.find(Beneficiaries.class, beneficiaryId);
+            return new ResponseEntity<>(createdBeneficiary, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping(consumes = "application/json", produces = "application/json")
+    public ResponseEntity<Beneficiaries> update(@RequestBody Beneficiaries beneficiary) {
+
+        if (beneficiary == null) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            beneficiary.setDateUpdated(new Date());
+            Beneficiaries updatedBeneficiary = service.update(beneficiary);
+            return new ResponseEntity<>(updatedBeneficiary, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
-

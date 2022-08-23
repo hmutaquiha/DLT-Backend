137c46ca2deac7c93ddc54858635e752128b6eb2
Dercio Bucuane <dercio.bucuane@fgh.org.mz>
2022-08-23 17:06:44 +0200

    include UsType entryPoint and impl Reference modifications

--- src/main/java/dlt/dltbackendmaster/controller/UsController.java
+++ src/main/java/dlt/dltbackendmaster/controller/UsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import dlt.dltbackendmaster.domain.ReferencesServices;
 import dlt.dltbackendmaster.domain.Us;
 import dlt.dltbackendmaster.service.DAOService;
 
@@ -36,7 +37,7 @@ public class UsController {
 		}
 	}
 
-	@GetMapping(path = "/{id}", consumes = "application/json")
+	@GetMapping(path = "/{id}", produces = "application/json")
 	public ResponseEntity<Us> get(@PathVariable Integer id){
 		if(id == null ) {
 			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
@@ -46,6 +47,24 @@ public class UsController {
 			Us us = service.find(Us.class, id);
 
 			return new ResponseEntity<>(us, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+	
+	@GetMapping(path = "/type/{typeId}/{localityId}", produces = "application/json")
+	public ResponseEntity<List<Us>> getByUsType(@PathVariable Integer typeId, @PathVariable Integer localityId){
+		
+		if(typeId == null || localityId == null) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}	
+		
+		try {
+			System.out.println("idt: "+typeId);
+			System.out.println("idt: "+localityId);
+			List<Us> uss = service.GetAllEntityByNamedQuery("Us.findByType", localityId, typeId);
+			
+			return new ResponseEntity<>(uss, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
 		}

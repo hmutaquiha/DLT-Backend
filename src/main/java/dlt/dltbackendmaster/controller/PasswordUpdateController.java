d8859d83dd2f17a8a91cdbef721d2210993b47ab
“Francisco <macuacuafrancisco@gmail.com>
2022-11-11 11:41:45 +0200

    Validar login de User inactivo e Colocar um link na página de confirmação da password que leva o utilizador a tela de login do DREAMS

--- src/main/java/dlt/dltbackendmaster/controller/PasswordUpdateController.java
+++ src/main/java/dlt/dltbackendmaster/controller/PasswordUpdateController.java
@@ -21,69 +21,68 @@ import net.bytebuddy.utility.RandomString;
 
 @Controller
 @RequestMapping("/users")
-public class PasswordUpdateController
-{
-    private final DAOService service;
-    private final PasswordEncoder passwordEncoder;
-    
-    @Autowired
-    private EmailSender emailSender;
+public class PasswordUpdateController {
+	private static final String ENV_HOST = "http://172.105.133.124:8080";
+	private final DAOService service;
+	private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    public PasswordUpdateController(DAOService service, PasswordEncoder passwordEncoder) {
-        this.service = service;
-        this.passwordEncoder = passwordEncoder;
-    }
+	@Autowired
+	private EmailSender emailSender;
 
-    @PutMapping(path = "/update-password", produces = "application/json")
-    public ResponseEntity<Users> updatePassword(HttpServletRequest request, @RequestBody Users users) {
+	@Autowired
+	public PasswordUpdateController(DAOService service, PasswordEncoder passwordEncoder) {
+		this.service = service;
+		this.passwordEncoder = passwordEncoder;
+	}
 
-        Users user = service.GetUniqueEntityByNamedQuery("Users.findByUsername", users.getUsername());
+	@PutMapping(path = "/update-password", produces = "application/json")
+	public ResponseEntity<Users> updatePassword(HttpServletRequest request, @RequestBody Users users) {
 
-        if (user == null) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        }
+		Users user = service.GetUniqueEntityByNamedQuery("Users.findByUsername", users.getUsername());
 
-        try {
-            user.setRecoverPassword(passwordEncoder.encode(users.getRecoverPassword()));
-            user.setNewPassword(0);
-            user.setIsEnabled(Byte.valueOf("0"));
-            String token = RandomString.make(45);
-            user.setRecoverPasswordToken(token);
-            Users updatedUser = service.update(user);
-            // Generate reset confirmation Link
-            String confirmUpdatePasswordLink = Utility.getSiteURL(request) + "/users/confirm-update?token=" + token;
-            // Send E-mail
-            emailSender.sendEmail(user.getName()+" "+user.getSurname(), user.getName() + " " + user.getSurname(),
-                                  null,
-                                  user.getEmail(),
-                                  confirmUpdatePasswordLink,
-                                  false);
-            return new ResponseEntity<>(updatedUser, HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+		if (user == null) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
 
-    @GetMapping(path = "/confirm-update")
-    public ResponseEntity<String> confirmPasswordUpdate(HttpServletRequest request, @Param(value = "token") String token) {
-        Users user = service.GetUniqueEntityByNamedQuery("Users.findByResetPasswordToken", token);
+		try {
+			user.setRecoverPassword(passwordEncoder.encode(users.getRecoverPassword()));
+			user.setNewPassword(0);
+			user.setIsEnabled(Byte.valueOf("0"));
+			String token = RandomString.make(45);
+			user.setRecoverPasswordToken(token);
+			Users updatedUser = service.update(user);
+			// Generate reset confirmation Link
+			String confirmUpdatePasswordLink = Utility.getSiteURL(request) + "/users/confirm-update?token=" + token;
+			// Send E-mail
+			emailSender.sendEmail(user.getName() + " " + user.getSurname(), user.getName() + " " + user.getSurname(),
+					null, user.getEmail(), confirmUpdatePasswordLink, false);
+			return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 
-        if (token == null || user == null) {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
-        }
+	@GetMapping(path = "/confirm-update")
+	public ResponseEntity<String> confirmPasswordUpdate(HttpServletRequest request,
+			@Param(value = "token") String token) {
+		Users user = service.GetUniqueEntityByNamedQuery("Users.findByResetPasswordToken", token);
 
-        try {
-            user.setPassword(user.getRecoverPassword());
-            user.setIsEnabled(Byte.valueOf("1"));
-            user.setRecoverPassword(null);
-            user.setRecoverPasswordToken(null);
-            Users updatedUser = service.update(user);
-            return new ResponseEntity<>("Confirmada a alteração da password do Utilizador " + updatedUser.getName()
-                                        + " " + user.getSurname() + "!",
-                                        HttpStatus.OK);
-        } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+		if (token == null || user == null) {
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+		}
+
+		try {
+			user.setPassword(user.getRecoverPassword());
+			user.setIsEnabled(Byte.valueOf("1"));
+			user.setRecoverPassword(null);
+			user.setRecoverPasswordToken(null);
+			Users updatedUser = service.update(user);
+			return new ResponseEntity<>(
+					"Confirmada a alteração da password do Utilizador " + updatedUser.getName() + " "
+							+ user.getSurname() + "!, " + " <a href=\"" + ENV_HOST + "/dreams#/login\">Login</a>",
+					HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
 }

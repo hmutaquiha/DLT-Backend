08ebc9544eeb0f2bf9cb66646f472f8eceb36d25
Francisco Macuacua <macuacuafrancisco@gmail.com>
2023-11-29 13:40:23 +0200

    remove credentials

--- src/main/java/dlt/dltbackendmaster/security/SecurityConfigurations.java
+++ src/main/java/dlt/dltbackendmaster/security/SecurityConfigurations.java
@@ -134,8 +134,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter{
         mailSender.setPort(587);
         // FIXME: Put valid credentials for e-mail and follow instructions on the link 
         // https://roytuts.com/gmail-smtp-and-security-settings-for-sending-email/
-        mailSender.setUsername("noreply@csaude.org.mz");
-        mailSender.setPassword(".123csaude4.");
+        mailSender.setUsername("change@email.here");
+        mailSender.setPassword("changepasswordhere");
 
         Properties javaMailProperties = new Properties();
         javaMailProperties.put("mail.smtp.auth", true);
